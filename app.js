6d71da7801d0178cba1771ebf12070f1f1d55506
Thach Ly Na B2205891 <nab2205891@student.ctu.edu.vn>
2025-10-18 13:59:35 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 const contactsRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/api-error');
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +12,17 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
-
 app.use('/api/contacts', contactsRouter);
+// handle 404 responses
+app.use((req, res,next) => {
+  return next(new ApiError(404, `Resource not found`));
+});
+
+app.use((err, req, res, next) => {
+
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error',
+    }); 
+});
 
 module.exports = app;
